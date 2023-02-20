95f05ab9783f108a0b77efea14dee2169d2860a7
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-02-20 14:31:39 +0100

    Initial Commit

---
+++ src/main/kotlin/com/tsola2002/phoneBook/PhoneBookApplication.kt
@@ -0,0 +1,11 @@
+package com.tsola2002.phoneBook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhoneBookApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhoneBookApplication>(*args)
+}
---
+++ src/test/kotlin/com/tsola2002/phoneBook/PhoneBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tsola2002.phoneBook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
